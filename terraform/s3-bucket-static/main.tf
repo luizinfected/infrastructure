6ca8b768c9f10e77a7d876010854445e16f3a0d6
Luiz <luiz7tenshi@gmail.com>
2025-03-07 16:40:22 -0300

    feat: add s3 infra

---
+++ terraform/s3-bucket-static/main.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+  region = "sa-east-1"
+}
+
+variable "bucket_name" {
+  type = string
+}
+
+resource "aws_s3_bucket" "static_site_bucket" {
+  bucket = "static-site-${var.bucket_name}"
+
+  tags = {
+    Name       = "Static Site Bucket"
+    Enviroment = "Production"
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "static_site_bucket" {
+  bucket = aws_s3_bucket.static_site_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "404.html"
+  }
+}
+
+
+resource "aws_s3_bucket_public_access_block" "static_site_bucket" {
+  bucket = aws_s3_bucket.static_site_bucket.id
+
+  block_public_acls = false
+  block_public_policy = false
+  ignore_public_acls = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "static_site_bucket" {
+  bucket = aws_s3_bucket.static_site_bucket.id
+  rule{
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "static_site_bucket" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.static_site_bucket,
+    aws_s3_bucket.static_site_bucket,
+  ]
+
+  bucket = aws_s3_bucket.static_site_bucket.id
+  acl    = "public-read"
+}
